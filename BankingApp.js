c29293c511d2f7028bf126adbf9d68d8d701f365
Amar kumar <amar66428@gmail.com>
2023-02-27 16:43:46 +0530

    initial commit

--- BankingApp.js
+++ BankingApp.js
@@ -1,6 +1,8 @@
 let prompt = require("prompt-sync")()
 console.log("<**********************{ Welcome to Apna Bank app }***********************>")
 
+
+
 let choice;
 let name;
 let id;
@@ -8,28 +10,26 @@ let phone;
 let address;
 let pincode;
 let Bank_bal = 0;
+let currentBal;
 let amount;
 var interestRate;
-var years;
+var month;
 var monthlyInterestRate;
-var months;
 var monthlyPayment; 
 
-function getAccNumber() {
-  return Math.floor(Math.random() * 10);
-  }
-
-const accountNo = getAccNumber();
  
 // ---------------------------------------------------------Creating new account-------------------------------------------
 
 function userDetails(){
+  Bank_bal = 0;
 
-name = prompt("Enter your full name:             ");
 var fullName = /^[a-z,',-]+(\s)[a-z,',-]+$/;
+name = prompt("Enter your full name:             ");
+
 if (!name.match(fullName)){
    console.log("Please enter your First and last name");
 }
+
 
 id = prompt("Enter your email id:              ")
 var validId = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -54,7 +54,23 @@ var validPin = /^[0-9]{6}?$/;
 if(!pincode.match(validPin)){
   console.log("Enter your valid area Pincode: ")
 }
+
+// ------------------------------------------------------creating Account number---------------------------------------
 }
+
+function generateAccountNumber() {
+  let accountNumber = '';
+  for (let i = 0; i < 8; i++) {
+    accountNumber += parseFloat(Math.floor(Math.random() * 10));
+  }
+  return accountNumber;
+}
+
+const accountNumber = generateAccountNumber();
+const accountNo = accountNumber;
+
+
+
 function userAcc(){
 
   console.log("<===================={  User Details  }==================>")
@@ -64,7 +80,6 @@ function userAcc(){
   console.log("Phone:           ",phone);
   console.log("Address:         ",address);
   console.log("Pincode:         ",pincode);
-  console.log("Current balance: ",Bank_bal)
   }
 
 
@@ -75,7 +90,8 @@ function deposit(){
     if(Acc==accountNo){ 
     let b = parseFloat(prompt("Enter amount for deposit: "));
     Bank_bal = Bank_bal + b;
-    console.log("Balance: ", Bank_bal);
+    console.log("--< Amount is added to your account >--")
+    console.log("Current balance: ", Bank_bal);
   }
   else{
     console.log("You have enter wrong input")
@@ -84,43 +100,76 @@ function deposit(){
 }
 
 //-------------------------------------------------------------------withdraw-------------------------------------
-function withdraw(){     
-     if( Bank_bal>=0 ){
+function withdraw(){ 
+  let Acc = prompt("Enter Account number: "); 
+  if(Acc==accountNo){   
+     if(Bank_bal>0){
      let c = prompt("Enter amount for withdraw: ");
-        Bank_bal = Bank_bal - c;
-     console.log("Balance: ", Bank_bal);
+     if(c<Bank_bal){
+     Bank_bal = Bank_bal - c;
+    console.log("Current Balance: ", Bank_bal);
      }
      else{
-      console.log("insufficient balance: ");
-     }    
+      console.log("--------------insufficient balance")
+     }
+     }
+    }    
+    else{
+      console.log("You have enter wrong input");
     }
+  }
+
 // -------------------------------------------------------------------loan---------------------------------------    
 function Loan(){
-  amount = parseFloat(prompt("loanAmount: "));
+  let Acc = prompt("Enter Account number: ");
+  if(Acc==accountNo){
+  amount = parseFloat(prompt("loan Amount: "));
   interestRate = 15;
-  years = parseFloat(prompt("Loan year: "));
-  monthlyInterestRate = interestRate / 1200;
-  months = years * 12;
-  monthlyPayment = (amount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -months));
-
+  month = parseFloat(prompt("Durration of months: "));
+  monthlyInterestRate = interestRate / 1500;
+  
+  monthlyPayment = (amount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -month));
+  
  console.log("Monthlyloan: ",monthlyPayment);
- console.log("Months: ",months);
- console.log("bank balance: ",Bank_bal + amount);
- 
+ console.log("Months: ",month);
+ currentBal = Bank_bal + amount;
+ console.log("Current balance: ",currentBal);
+
+  }
+  else{
+    console.log("You have enter wrong input");
+  }
 }
+function world(){
+  console.log("current Balance:", Bank_bal);
+}
+
 function payEmi(){
+  let Acc = prompt("Enter Account number: ");
+  if(Acc==accountNo){
   if(Bank_bal<0){
     console.log("Insufficient Balance! ");
   }
   else{
-    months--;
-    Bank_bal = Bank_bal - monthlyPayment;
+    if(month<=0){
+      console.log("<------You have not any emi is panding")
+    }
+    month--;
+    currentBal = currentBal - monthlyPayment;
   console.log("=> Your Emi is paid successfully ");
 }
+  }
+  else{
+    console.log("You have enter wrong input");
+  }
+}
+
+function currentbal(){
+  console.log("Current balance:",currentBal);
 }
 
 function loanDetail(){
-console.log("Total months left:",months);
+console.log("Total months left:",month);
 console.log("Monthly Emi: ",monthlyPayment);
 }
 
@@ -130,7 +179,7 @@ console.log("Press 3 to withdraw money:");
 console.log("Press 4 to take load for car, home and education :");
 console.log("Press 5 to pay Emi :");
 console.log("Press 6 to See Acount details:")
-console.log("Press 0 to Exit")
+console.log("                              ");
 
 do{
   choice = prompt("Enter your choice (0 to Exit)=> ");
@@ -139,6 +188,8 @@ switch (choice)
   case '1': 
     userDetails();
     userAcc();
+    world();
+    
     break;
 
   case '2':
@@ -159,8 +210,11 @@ switch (choice)
 
   case '6':
     userAcc();
+    currentbal();
     loanDetail(); 
   break;
+  default:
+    console.log("Invalid input");
   }
 }while (choice !== '0');
 console.log("<~~~~~~~~~~~~~~~~~~~~~~~{ Thank you for kind visit to World bank }~~~~~~~~~~~~~~~~~~~~~~>")
