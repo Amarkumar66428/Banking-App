81cc79355fa58bf665f927ea2279b69bb35c6354
Amar kumar <amar66428@gmail.com>
2023-02-27 11:11:05 +0530

    new commit

--- BankingApp.js
+++ BankingApp.js
@@ -1,94 +1,166 @@
 let prompt = require("prompt-sync")()
-console.log("**********************{ Welcome to Apna Bank app }***********************")
+console.log("<**********************{ Welcome to Apna Bank app }***********************>")
 
+let choice;
+let name;
+let id;
+let phone;
+let address;
+let pincode;
+let Bank_bal = 0;
+let amount;
+var interestRate;
+var years;
+var monthlyInterestRate;
+var months;
+var monthlyPayment; 
+ 
 
-console.log("Press 1 to register new user:");
+console.log("Press 1 to create new Account:");
 console.log("Press 2 to deposit money:"); 
 console.log("Press 3 to withdraw money: ");
 console.log("Press 4 to take load for car, home and education : ");
+console.log("Press 5 to pay Emi : ");
+console.log("Press 6 to ")
 console.log("Press 0 to Exit")
-let x = prompt("=> ");
+choice = prompt("=> ");
 
 // ---------------------------------------------------------Register-------------------------------------------
- 
-while(x!==0){
+
+
 function userDetails(){
-let name = prompt("Enter your full name: ");
+
+name = prompt("Enter your full name:             ");
 var fullName = /^[a-z,',-]+(\s)[a-z,',-]+$/;
-if(!name.match(fullName)){
+if (!name.match(fullName)){
    console.log("Please enter your First and last name");
 }
 
-let id = prompt("Enter your email id: ")
+id = prompt("Enter your email id:              ")
 var validId = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 if(!id.match(validId)){
    console.log("Invalid Email");
 }
 
-let phone = prompt("Enter your phone number: ");
+phone = prompt("Enter your phone number:          ");
 var validPh = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
 if(!phone.match(validPh)){
   console.log("Your entered phone number is invalid ")
 }
 
-let address = prompt("Enter your address: ");
+address = prompt("Enter your address:               ");
 var validAd = /^[a-zA-Z0-9-\/] ?([a-zA-Z0-9-\/]|[a-zA-Z0-9-\/] )*[a-zA-Z0-9-\/]$/;
 if(!address.match(validAd)){
   console.log("Enter your valid address: ")
 }
 
-let pincode = prompt("Enter your area pincode: ");
+pincode = prompt("Enter your area pincode:          ");
 var validPin = /^[0-9]{6}?$/;
 if(!pincode.match(validPin)){
   console.log("Enter your valid area Pincode: ")
 }
 }
+function userAcc(){
+
+  console.log("<===================={  User Details  }==================>")
+  console.log("Name:            ",name);
+  console.log("Email id:        ",id);
+  console.log("Phone:           ",phone);
+  console.log("Address:         ",address);
+  console.log("Pincode:         ",pincode);
+  console.log("Current balance: ",Bank_bal)
+  }
+
 
 // -----------------------------------------------------------------Deposit-------------------------------------
-var a = 0 ;
 function deposit(){   
-    let b = prompt("Enter amount for deposit: ");
-    a = a + b;
-    console.log("Balance: ", a);
+    let b = parseFloat(prompt("Enter amount for deposit: "));
+    Bank_bal = Bank_bal + b;
+    console.log("Balance: ", Bank_bal);
 }
 
 //-------------------------------------------------------------------withdraw-------------------------------------
 function withdraw(){     
-     if(!a==0){
+     if( Bank_bal>=0 ){
      let c = prompt("Enter amount for withdraw: ");
-        a = a - c;
-     console.log("Balance: ", a );
+        Bank_bal = Bank_bal - c;
+     console.log("Balance: ", Bank_bal);
      }
      else{
       console.log("insufficient balance: ");
      }    
     }
-// -------------------------------------------------------------------load---------------------------------------    
-function carLoan(){
- let amount = prompt("Enter amount you want to take: ");
- let months = prompt("For much duration of months: ");
- let interest = 14;
- let total = ((amount/months) + interest);
- console.log("Total loan: ",total);
-}
+// -------------------------------------------------------------------loan---------------------------------------    
+function Loan(){
+  amount = parseFloat(prompt("loanAmount: "));
+  interestRate = 15;
+  years = parseFloat(prompt("Loan year: "));
+  monthlyInterestRate = interestRate / 1200;
+  months = years * 12;
+  monthlyPayment = (amount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -months));
 
-switch (x) {
+ console.log("Monthlyloan: ",monthlyPayment);
+ console.log("Months: ",months);
+ console.log("bank balance: ",Bank_bal + amount);
+ 
+}
+function payEmi(){
+  if(Bank_bal<0){
+    console.log("Insufficient Balance! ");
+  }
+  else{
+  console.log(months-1);
+  console.log(Bank_bal-monthlyPayment);
+}
+}
+function loanDetail(){
+
+console.log("Total months left:",months);
+console.log("Monthly Emi: ",monthlyPayment);
+}
+function handleInput() {
+switch (choice)
+ {
   case '0':
+    console.log('Exiting program.');
     break;
-  case '1':
+
+  case '1': 
     userDetails();
+    userAcc();
     break;
+
   case '2':
     deposit();
     break;
+
   case '3':
     withdraw();
-  break;
   case '4':
-    carLoan();
+    Loan();
   break;
-  default:
-    console.log("Invalid button! Thanks for using Apna Bank app");
+
+  case '5':
+    payEmi ();  
+  break;
+
+  case '6':
+    userAcc();
+    loanDetail(); 
+  break;
+
+    default:
+      console.log('Invalid input');
+      break;
+  }
 }
-}
+// var Bank = handleInput();
+  
+if (choice !== '0') {
+  choice =  prompt('Enter a number (0 to exit): ');
+  handleInput();
+  }
+  // prompt('Enter a number (0 to exit): ', Bank);
+
+
 
